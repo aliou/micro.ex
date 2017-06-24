1cea0fb48947b16f55630bcd35e5f925315fb7f6
Aliou Diallo <code@aliou.me>
2017-06-24 19:22:51 +0200

    Create Micro router.
    For now, let's 404 all of the requests. The actual route will be
    implemented in other plugs and dispatched from this router with custom
    options.

--- lib/micro.ex
+++ lib/micro.ex
@@ -1,2 +1,13 @@
 defmodule Micro do
+  defmodule Router do
+    use Plug.Router
+
+    plug Plug.Logger
+    plug :match
+    plug :dispatch
+
+    match _ do
+      send_resp(conn, 404, "")
+    end
+  end
 end
