f95a573d4a223d58fea56efba465ff1bbffbfdfc
Aliou Diallo <code@aliou.me>
2017-06-24 15:26:36 +0200

    Setup concurrent tests.

--- config/test.exs
+++ config/test.exs
@@ -3,4 +3,5 @@ use Mix.Config
 config :micro, Micro.Repo,
   adapter: Ecto.Adapters.Postgres,
   database: "micro_test",
-  hostname: "localhost"
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
