4924c0d3a42efcf6a0499dcdae0605c1da9c7076
Aliou Diallo <code@aliou.me>
2017-06-28 22:48:43 +0200

    Create the Micro Feedbuilder.

---
+++ lib/micro/feed_builder.ex
@@ -0,0 +1,44 @@
+defmodule Micro.FeedBuilder do
+  @moduledoc """
+  Feedbuilder for the Micro Application. This implements the Feed Builder
+  behavior.
+
+  TODO: I think this still needs work and tests.
+  """
+  @behaviour Feed.Builder
+
+  import Ecto.Query, only: [order_by: 2]
+
+  @impl Feed.Builder
+  def build_feed do
+    # TODO: Move this into config file?
+    %Feed{
+      title: "Aliou's Micro blog",
+      feed_url: "https://micro.aliou.me/feed.json",
+      home_page_url: "https://micro.aliou.me"
+    }
+  end
+
+  @impl Feed.Builder
+  def build_entries do
+    fetch_posts()
+    |> Enum.map(&entry_from_post/1)
+  end
+
+  defp fetch_posts do
+    Micro.Post |> order_by(desc: :inserted_at) |> Micro.Repo.all()
+  end
+
+  defp entry_from_post(post) do
+    content_html = Micro.Post.content_to_html(post)
+    date_modified =
+      if post.inserted_at != post.updated_at, do: post.updated_at, else: nil
+
+      %Feed.Entry{
+        id: post.id,
+        content_html: content_html,
+        date_published: post.inserted_at,
+        date_modified: date_modified
+      }
+  end
+end
