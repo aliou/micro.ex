dbb7afa491e66d9cb712fdb637b43725cbe4a385
Aliou Diallo <code@aliou.me>
2017-07-12 20:01:29 +0200

    Add example for Feed Builder behaviour.

--- lib/feed/builder.ex
+++ lib/feed/builder.ex
@@ -1,6 +1,28 @@
 defmodule Feed.Builder do
   @moduledoc """
-  Behaviour defining what is a Feed builder
+  Behaviour for creating a Feed builder.
+
+  ## Example
+        defmodule Feed.CustomBuilder do
+          @behaviour Feed.Builder
+
+          @impl Feed.Builder
+          def build_feed do
+            %Feed{
+              title: "My Custom Feed",
+            }
+          end
+
+          @impl Feed.Builder
+          def build_items do
+            [
+              %Feed.Item{
+                id: "this-is-an-id",
+                content_html: "<h1>This a some content</h1>"
+              }
+            ]
+          end
+        end
   """
 
   @callback build_items() :: [Feed.Item.t]
