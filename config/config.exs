ab75d798b345780897a4ed8eb21d3a5a5b842178
Aliou Diallo <code@aliou.me>
2018-04-13 06:54:51 +0200

    Set default port.

--- config/config.exs
+++ config/config.exs
@@ -8,7 +8,7 @@ config :micro, ecto_repos: [Micro.Repo]
 # Tell the router what port to run on. This is overriden in the test
 # environemnt.
 config :micro, Micro.Router,
-  port: String.to_integer(System.get_env("PORT"))
+  port: String.to_integer(System.get_env("PORT") || "4000")
 
 # Per environment configuration.
 import_config "#{Mix.env}.exs"
