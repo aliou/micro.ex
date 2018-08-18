cf8c821e48fbc32ed3e9d0ea6a5a6db376aff8f5
Aliou Diallo <code@aliou.me>
2018-08-18 09:23:03 +0200

    Use templates module.

--- lib/micro/post/router.ex
+++ lib/micro/post/router.ex
@@ -2,28 +2,39 @@ defmodule Micro.Post.Router do
   use Plug.Router
 
   import Micro.Router.Utils, only: [not_found: 1, resp_html: 2]
-  alias Micro.Post
-
-  if Mix.env() != :test do
-    plug Logster.Plugs.Logger
-  end
 
   plug :match
   plug :dispatch
 
   get "/" do
-    resp_html(conn, "<h3>A list of posts</h3>")
+    posts =
+      conn
+      |> extract_page_number
+      |> Micro.Posts.paginated_posts()
+
+    render(conn, "posts", posts: posts, pagination: %{})
   end
 
   # TODO: Redirect to the slug if present ?
   get "/:id" do
     case Micro.Posts.friendly_find(conn.params["id"]) do
-      :not_found -> not_found(conn)
-      {:ok, post} -> resp_html(conn, Post.content_to_html(post))
+      :not_found ->
+        not_found(conn)
+
+      {:ok, post} ->
+        render(conn, "post", post)
     end
   end
 
-  match _ do
-    not_found(conn)
+  match _, do: not_found(conn)
+
+  # TODO: better than this pls
+  defp extract_page_number(conn) do
+    conn = Plug.Conn.fetch_query_params(conn)
+    Map.get(conn.query_params, "page", "1") |> String.to_integer()
+  end
+
+  defp render(conn, template, variable) do
+    conn |> resp_html(Micro.Template.render(template, variable))
   end
 end
